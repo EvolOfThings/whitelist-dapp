827e4c55658add86782683046c27141f273c1568
rishad <rishad.yamnoor@gmail.com>
2023-06-24 21:29:11 +0530

    nft contract and deploy

---
+++ hardhat/scripts/deploy-nft.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  // Deploy the CryptoDevs Contract
+  const nftContract = await hre.ethers.deployContract("CryptoDevs", [
+    "0xb5c3E7452710ba6FE6E5B9365d8de4d2c0fcF7Bd",
+  ]);
+
+  // wait for the contract to deploy
+  await nftContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", nftContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: ["0xb5c3E7452710ba6FE6E5B9365d8de4d2c0fcF7Bd"],
+  });
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
